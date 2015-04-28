cee5c10a99a9875e6b2ecb12c3103c43da54e8c0
manoje-p <mprtthisathaporn2015@kellogg.northwestern.edu>
2015-04-28 00:36:05 -0500

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Card.delete_all
+Guest.delete_all
+
+manoje = User.create(name: "Manoje")
+hanne = User.create(name: "Hanne")
+emily = User.create(name: "Emily")
+reema = User.create(name: "Reema")
+dan = User.create(name: "Dan")
+
+one = Card.create(sku: 1)
+two = Card.create(sku: 2)
+three = Card.create(sku: 3)
+four = Card.create(sku: 4)
+five = Card.create(sku: 5)
+
+Guest.create(user_id: manoje.id, name: "Nick", address: "1500 Chicago Ave.", gift: "Handmade", status: "Done", card_id: one.id)
+Guest.create(user_id: manoje.id, name: "Pang", address: "1500 Chicago Ave.", gift: "Postcard", status: "Done", card_id: two.id)
+Guest.create(user_id: manoje.id, name: "Jess", address: "1500 Chicago Ave.", gift: "Grocery", status: "Done", card_id: two.id)
+Guest.create(user_id: manoje.id, name: "Emily", address: "1600 Hinman Ave.", gift: "Wine Glass", status: "Done", card_id: two.id)
+Guest.create(user_id: manoje.id, name: "Pai", address: "1410 Chicago Ave.", gift: "Heater", status: "Done", card_id: two.id)
